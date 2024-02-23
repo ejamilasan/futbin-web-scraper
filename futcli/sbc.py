e3c3a9c071683a6b5381e38f9f58097e8d134846
ejamilasan <ejamilasan@gmail.com>
2024-02-23 17:37:54 -0600

    remove main from sbc.py

--- futcli/sbc.py
+++ futcli/sbc.py
@@ -58,22 +58,3 @@ def get_sbc(option):
 
         return sbc_data
     return []
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        if len(sys.argv) == 2 and sys.argv[1].lower() == 'sbc':
-            sbc_options = get_sbc_options()
-            print(json.dumps(sbc_options, indent=4))
-        else:
-            print("Usage: python3 fut_scraper.py sbc players")
-        sys.exit(1)
-
-    category = sys.argv[1].lower()
-    option = sys.argv[2].lower()
-
-    if category == 'sbc':
-        sbc_data = get_sbc(option)
-        if sbc_data:
-            print(json.dumps(sbc_data, indent=4))
-        else:
-            print("No SBC data available.")
