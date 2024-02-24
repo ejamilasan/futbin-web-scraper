455ad4f436d1e33fe61a196a3870b7de66c42cb5
ejamilasan <ejamilasan@gmail.com>
2024-02-24 14:22:48 -0600

    make futcli pretty

--- futcli/futcli.py
+++ futcli/futcli.py
@@ -1,17 +1,41 @@
 import sys
 import json
 import argparse
-
 from tabulate import tabulate
-
 from sbc import get_sbc_types, get_sbc_item_properties, get_sbc_items
 from evolutions import get_evolution_item_properties, get_evolution_items
 
-def futcli():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter
-    )
+def format_output(data, output_format):
+    """Format the output to either json or table."""
+    if output_format == 'json':
+        print(json.dumps(data, indent=4))
+    else:
+        print(tabulate(data, headers='keys', tablefmt='grid'))
 
+def get_output(data_type, option, output_format):
+    """Retrieve data based on the specified type and option."""
+    if data_type == 'sbc':
+        sbc_data = get_sbc_items()
+        if option is None:
+            combined_data = [item for sublist in sbc_data.values() for item in sublist]
+            format_output(combined_data, output_format)
+        elif option in sbc_data:
+            format_output(sbc_data[option], output_format)
+        else:
+            print("Invalid SBC option.")
+    elif data_type == 'evolutions':
+        evolutions_data = get_evolution_items()
+        formatted_evolutions = []
+        for item in evolutions_data:
+            formatted_item = {key: (json.dumps(value, indent=4) if isinstance(value, dict) else value) for key, value in item.items()}
+            formatted_evolutions.append(formatted_item)
+        format_output(formatted_evolutions, output_format)
+    else:
+        print("Invalid data type.")
+
+def futcli():
+    """Command-line interface for fetching FIFA Ultimate Team data."""
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(
         "-o", "--output",
         choices=["table", "json"],
@@ -22,51 +46,19 @@ def futcli():
     subparsers = parser.add_subparsers(dest='data_type', title='args', metavar="[sbc.{options}, evolutions]")
     for sbc_type in get_sbc_types():
         sbc_parser = subparsers.add_parser(f"sbc.{sbc_type}", help=f'Outputs list of SBC {sbc_type}')
-    subparsers.add_parser("sbc.all", help='Outputs list of all SBC types')
+    subparsers.add_parser("sbc", help='Outputs list of all SBC types')
     subparsers.add_parser("evolutions", help='Outputs list of all active Evolutions')
 
     args = parser.parse_args()
 
     if args.data_type:
-        if args.data_type.startswith('sbc'):
+        if '.' in args.data_type:
             data_type, option = args.data_type.split('.')
         else:
             data_type = args.data_type
+            option = None
 
-        sbc_data = get_sbc_items()
-        evolutions_data = get_evolution_items()
-
-        if data_type == 'sbc':
-            combined_data = []
-
-            if option == 'all':
-                for data_list in sbc_data.values():
-                    combined_data.extend(data_list)
-
-                    if args.output == 'json':
-                        print(json.dumps(combined_data, indent=4))
-                    else:
-                        print(tabulate(combined_data, headers='keys', tablefmt='grid'))
-            else:
-                if args.output == 'json':
-                    print(json.dumps(sbc_data[option], indent=4))
-                else:
-                    print(tabulate(sbc_data[option], headers='keys', tablefmt='grid'))
-        elif data_type == 'evolutions':
-            if args.output == 'json':
-                print(json.dumps(evolutions_data, indent=4))
-            else:
-                tabulated_data = []
-                for item in evolutions_data:
-                    tabulated_item = {}
-                    for key, value in item.items():
-                        if isinstance(value, dict):
-                            tabulated_item[key] = json.dumps(value, indent=4)
-                        else:
-                            tabulated_item[key] = value
-                    tabulated_data.append(tabulated_item)
-
-                print(tabulate(tabulated_data, headers='keys', tablefmt='grid'))
+        get_output(data_type, option, args.output)
 
 if __name__ == "__main__":
     futcli()
