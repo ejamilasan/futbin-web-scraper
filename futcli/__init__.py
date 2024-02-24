121ac9f674a7836a039806691bdbc6443da86e85
ejamilasan <ejamilasan@gmail.com>
2024-02-24 17:24:10 -0600

    bump to 0.1.2

--- futcli/__init__.py
+++ futcli/__init__.py
@@ -1,7 +1,7 @@
 # futcli/__init__.py
 
 __title__ = "futcli"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Edwin V. Amilasan Jr."
 __author_email__ = "ejamilasan@gmail.com"
 __license__ = "GNU GPL v3"
